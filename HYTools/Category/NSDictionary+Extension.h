f93d7d32eb4a971974a0cb15ff8d466c78f9e6bd
huyang <982189800@qq.com>
2017-05-19 14:03:22 +0800

    update
    清除字典里的特殊字符@"<null>”等，防止崩溃

---
+++ HYTools/Category/NSDictionary+Extension.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+Extension.h
+//  HYTools
+//
+//  Created by 胡杨 on 2017/5/11.
+//  Copyright © 2017年 net.fitcome.www. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Extension)
+
+- (id)processDictionaryIsNSNull:(id)obj;
+
+@end
---
+++ HYTools/Category/NSDictionary+Extension.m
@@ -0,0 +1,58 @@
+//
+//  NSDictionary+Extension.m
+//  HYTools
+//
+//  Created by 胡杨 on 2017/5/11.
+//  Copyright © 2017年 net.fitcome.www. All rights reserved.
+//
+
+#import "NSDictionary+Extension.h"
+
+@implementation NSDictionary (Extension)
+
+- (id)processDictionaryIsNSNull:(id)obj {
+    const NSString *blank = @"";
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        NSMutableDictionary *dt = [(NSMutableDictionary*)obj mutableCopy];
+        for(NSString *key in [dt allKeys]) {
+            id object = [dt objectForKey:key];
+            if([object isKindOfClass:[NSNull class]]) {
+                [dt setObject:blank
+                       forKey:key];
+            }
+            else if ([object isKindOfClass:[NSString class]]){
+                NSString *strobj = (NSString*)object;
+                if ([strobj isEqualToString:@"<null>"]) {
+                    [dt setObject:blank
+                           forKey:key];
+                }
+            }
+            else if ([object isKindOfClass:[NSArray class]]){
+                NSArray *da = (NSArray*)object;
+                da = [self processDictionaryIsNSNull:da];
+                [dt setObject:da
+                       forKey:key];
+            }
+            else if ([object isKindOfClass:[NSDictionary class]]){
+                NSDictionary *ddc = (NSDictionary*)object;
+                ddc = [self processDictionaryIsNSNull:object];
+                [dt setObject:ddc forKey:key];
+            }
+        }
+        return [dt copy];
+    }
+    else if ([obj isKindOfClass:[NSArray class]]){
+        NSMutableArray *da = [(NSMutableArray*)obj mutableCopy];
+        for (int i=0; i<[da count]; i++) {
+            NSDictionary *dc = [obj objectAtIndex:i];
+            dc = [self processDictionaryIsNSNull:dc];
+            [da replaceObjectAtIndex:i withObject:dc];
+        }
+        return [da copy];
+    }
+    else{
+        return obj;
+    }
+}
+
+@end
